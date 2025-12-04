57533aee27b872da50482db2752770412dd7ad6d
Jakub Gwóźdź <jakub.gwozdz@gmail.com>
2025-12-04 15:27:09 +0100

    day4 v2.1

--- src/main/kotlin/aoc2025/day4/Day4.kt
+++ src/main/kotlin/aoc2025/day4/Day4.kt
@@ -1,7 +1,6 @@
 package aoc2025.day4
 
 import catching
-import debug
 import eventAndDayFromPackage
 import go
 import helpers.*
@@ -17,28 +16,25 @@ fun main() = catching {
 fun part1(data: String): Any = parse(data).sumOf { it.count { it in 0..<4 } }
 
 fun part2(data: String): Any {
-    val arrays = parse(data)
-    var toRemove = buildList {
-        arrays.forEachIndexed { row, line ->
-            line.forEachIndexed { col, v ->
-                if (v in 0..<4) add(longPos(row, col)).also { arrays[row][col] = -1 }
-            }
+    val arrays = parse(data) // List<IntArray> (0..8 - number of adjacent rolls, -1 - no roll here)
+    val removed = mutableListOf<LongPos>()
+
+    arrays.forEachIndexed { row, line ->
+        line.forEachIndexed { col, v ->
+            if (v in 0..<4) removed.add(longPos(row, col)).also { arrays[row][col] = -1 }
         }
     }
-    var result = toRemove.size
-    while (toRemove.isNotEmpty()) {
-        toRemove = buildList {
-            toRemove.forEach { pos ->
-                pos.neighbours8().forEach { p1 ->
-                    val (r1, c1) = p1
-                    if (r1 in arrays.indices && c1 in arrays[r1].indices && arrays[r1][c1] > 3) {
-                        arrays[r1][c1]--
-                        if (arrays[r1][c1] == 3) add(p1).also { arrays[r1][c1] = -1 }
-                    }
-                }
+    var result = 0
+    while (removed.isNotEmpty()) {
+        val pos = removed.removeLast()
+        result++
+        pos.neighbours8().forEach { p1 ->
+            val (r1, c1) = p1
+            if (r1 in arrays.indices && c1 in arrays[r1].indices && arrays[r1][c1] > 3) {
+                arrays[r1][c1]--
+                if (arrays[r1][c1] == 3) removed.add(p1).also { arrays[r1][c1] = -1 }
             }
         }
-        result += toRemove.size
     }
     return result
 }
