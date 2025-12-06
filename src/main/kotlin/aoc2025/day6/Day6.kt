3efa463568d8fd7ef59545c6ba7c99bd6fa96213
Jakub Gwóźdź <jakub.gwozdz@gmail.com>
2025-12-06 07:08:55 +0100

    day 6

--- src/main/kotlin/aoc2025/day6/Day6.kt
+++ src/main/kotlin/aoc2025/day6/Day6.kt
@@ -1,7 +1,6 @@
 package aoc2025.day6
 
 import catching
-import debug
 import go
 import eventAndDayFromPackage
 import provideInput
@@ -10,41 +9,33 @@ fun main() = catching {
     val (event, day) = eventAndDayFromPackage { }
     val input = provideInput(event, day)
     go("part 1", 5782351442566) { part1(input) }
-    val p2ex = """
-        123 328  51 64 
-         45 64  387 23 
-          6 98  215 314
-        *   +   *   +  
-    """.trimIndent()
-    go("part2ex1", 3263827) { part2(p2ex) }
     go("part 2", 10194584711842) { part2(input) }
 }
 
-fun part1(data: String): Any {
-    val pieces = data.reader().readLines().map { it.split(Regex("\\s+")).filter(String::isNotBlank) }
-    val arguments = pieces.dropLast(1).map { it.map(String::toLong) }
-    return pieces.last().mapIndexed { index, string ->
-        val op = string[0]
-        val args = arguments.map { it[index] }
-        calc(op, args)
-    }.sum()
+fun part1(data: String) = solve(data) { strings ->
+    strings.map { it.trim().toLong() }
 }
 
-private fun calc(op: Char, args: List<Long>): Long = when (op) {
-    '+' -> args.sum()
-    '*' -> args.fold(1L) { acc, list -> acc * list }
-    else -> error("Unknown op $op")
+fun part2(data: String) = solve(data) { strings ->
+    strings.transposed().map { it.trim().toLong() }
 }
 
-fun part2(data: String): Any {
+private fun List<String>.transposed(): List<String> =
+    first().indices.map { c -> indices.map { r -> this[r][c] }.joinToString("") }
+
+private fun solve(data: String, numbersOp: (List<String>) -> List<Long>): Long {
     val lines = data.reader().readLines()
-    val parts = lines.last().withIndex().filter { it.value != ' ' }.map { it.index }.let {
-        it.windowed(2).map { (a, b) -> a..<b-1 } + listOf(it.last()..<lines.last().length) }
+    val ranges = lines.last().withIndex().filter { it.value != ' ' }.map { it.index }
+        .let { it.windowed(2).map { (a, b) -> a..<b - 1 } + listOf(it.last()..<lines.last().length) }
+    val parts = ranges.map { range -> lines.map { line -> line.substring(range) } }
 
-    return parts.sumOf { range ->
-        val args = range.map { c->(0..<lines.lastIndex).map { r->lines[r][c] }.joinToString("") }
-            .filter { it.isNotBlank() }.map { it.trim().toLong() }
-        val op = lines.last()[range.first]
-        calc(op, args)
+    return parts.sumOf { part ->
+        val args = numbersOp(part.dropLast(1))
+        when (part.last().first()) {
+            '+' -> args.sum()
+            '*' -> args.fold(1L) { acc, list -> acc * list }
+            else -> error("Unknown op")
+        }
     }
 }
+
