a2df0ea7a0f1dba61cc25fa6da0f333583f8a58b
Jakub Gwóźdź <jakub.gwozdz@gmail.com>
2025-12-09 11:57:59 +0100

    day 9

--- src/main/kotlin/aoc2025/day9/Day9.kt
+++ src/main/kotlin/aoc2025/day9/Day9.kt
@@ -48,57 +48,51 @@ fun part1(data: String): Any {
 data class Rect(
     val p1: Pos,
     val p2: Pos,
-    val xInside: IntRange,
-    val yInside: IntRange,
     val size: Long
 )
 
 fun part2(data: String): Any {
     val reds = data.reader().readLines().map { it.split(",").let { (a, b) -> Pos(a.toInt(), b.toInt()) } }
-    val (hLines, vLines) = (reds.zipWithNext() + (reds.last() to reds.first()))
-        .map { (p1, p2) ->
-            if (p1.x == p2.x) (p1.x .. p1.x) to (p1.y.coerceAtMost(p2.y)..p1.y.coerceAtLeast(p2.y))
-            else if (p1.y == p2.y) (p1.x.coerceAtMost(p2.x) .. p1.x.coerceAtLeast(p2.x)) to (p1.y .. p1.y)
-            else error("impossible")
-        }
-        .partition { (p1, p2) -> p1.first != p1.last }
+    val greens = (reds.zipWithNext() + (reds.last() to reds.first())).map { (p1, p2) ->
+        if (p1.x == p2.x) (p1.x..p1.x) to (p1.y.coerceAtMost(p2.y)..p1.y.coerceAtLeast(p2.y))
+        else if (p1.y == p2.y) (p1.x.coerceAtMost(p2.x)..p1.x.coerceAtLeast(p2.x)) to (p1.y..p1.y)
+        else error("impossible")
+    }
+
+    val hLines = greens.filter { (p1, p2) -> p1.first != p1.last }
+        .map { (xs, ys) -> ys.single() to xs }
+
+    val vLines = greens.filter { (p1, p2) -> p1.first == p1.last }
+        .map { (xs, ys) -> xs.single() to ys }
 
     val rects = reds.flatMapIndexed { i, p1 ->
         val (x1, y1) = p1
         reds.subList(0, i).map { p2 ->
             val (x2, y2) = p2
-            val xs = x1.coerceAtMost(x2) + 1..x1.coerceAtLeast(x2) - 1
-            val ys = y1.coerceAtMost(y2) + 1..y1.coerceAtLeast(y2) - 1
-            Rect(p1, p2, xs, ys, (xs.last - xs.first + 3L) * (ys.last - ys.first + 3L))
+            val p1a = Pos(x1.coerceAtMost(x2), y1.coerceAtMost(y2))
+            val p2a = Pos(x1.coerceAtLeast(x2), y1.coerceAtLeast(y2))
+            Rect(p1a, p2a, (p2a.x - p1a.x + 1L) * (p2a.y - p1a.y + 1L))
         }
     }.filter { (p1, p2) -> p1.x != p2.x && p1.y != p2.y }
 
     return rects.filter { rect ->
-        val minX = min(rect.p1.x, rect.p2.x) + 1
-        val minY = min(rect.p1.y, rect.p2.y) + 1
-        val maxX = max(rect.p1.x, rect.p2.x) - 1
-        val maxY = max(rect.p1.y, rect.p2.y) - 1
+        val minX = rect.p1.x + 1
+        val minY = rect.p1.y + 1
+        val maxX = rect.p2.x - 1
+        val maxY = rect.p2.y - 1
 
-        val hBorders1 = hLines.filter { (range, _) -> range.first < minX && minX < range.last }
-        val countMinY1 = hBorders1.count { (_, range) -> range.first < minY }
-        val countMaxY1 = hBorders1.count { (_, range) -> range.first < maxY }
-        if (countMinY1 % 2 != 1 || countMaxY1 != countMinY1) return@filter false
-
-        val hBorders2 = hLines.filter { (range, _) -> range.first < maxX && maxX < range.last }
-        val countMinY2 = hBorders2.count { (_, range) -> range.first < minY }
-        val countMaxY2 = hBorders2.count { (_, range) -> range.first < maxY }
-        if (countMinY2 % 2 != 1 || countMaxY2 != countMinY2) return@filter false
-
-        val vBorders1 = vLines.filter { (_, range) -> range.first < minY && minY < range.last }
-        val countMinX1 = vBorders1.count { (range, _) -> range.first < minX }
-        val countMaxX1 = vBorders1.count { (range, _) -> range.first < maxX }
-        if (countMinX1 % 2 != 1 || countMaxX1 != countMinX1) return@filter false
-
-        val vBorders2 = vLines.filter { (_, range) -> range.first < maxY && maxY < range.last }
-        val countMinX2 = vBorders2.count { (range, _) -> range.first < minX }
-        val countMaxX2 = vBorders2.count { (range, _) -> range.first < maxX }
-        if (countMinX2 % 2 != 1 || countMaxX2 != countMinX2) return@filter false
+        if (!valid(hLines, minX, minY, maxY)) return@filter false
+        if (!valid(hLines, maxX, minY, maxY)) return@filter false
+        if (!valid(vLines, minY, minX, maxX)) return@filter false
+        if (!valid(vLines, maxY, minX, maxX)) return@filter false
 
         true
     }.maxOf { it.size }
+}
+
+private fun valid(lines: List<Pair<Int, IntRange>>, main: Int, otherMin: Int, otherMax: Int): Boolean {
+    val filtered = lines.filter { (_, range) -> range.first < main && main < range.last }
+    val countMinY1 = filtered.count { (r, _) -> r < otherMin }
+    val countMaxY1 = filtered.count { (r, _) -> r < otherMax }
+    return countMinY1 % 2 == 1 && countMaxY1 == countMinY1
 }
